a82ea46516658725865bfdf07bbf95e5c567d1df
Breno Luiz P. Leal <brenoluizleal@gmail.com>
2022-04-11 20:03:26 -0300

    Criação do método POST no data.service.ts

--- samha-frontend/src/app/shared/data.service.ts
+++ samha-frontend/src/app/shared/data.service.ts
@@ -5,10 +5,16 @@ import {Observable} from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class DataService {
 
+  private static APIPREFIX = 'api/';
+
   constructor(private http: HttpClient) {}
 
   public get(resource: string, id: string): Observable<any>{
-    return this.http.get(resource + '/' + id);
+    return this.http.get(DataService.APIPREFIX + resource + '/' + id);
+  }
+
+  public post(resource: string, body: any): Observable<any>{
+    return this.http.post(DataService.APIPREFIX + resource, body);
   }
 
 }
