e7222e2893e744d6e351f22288f62fb62428a6bb
Breno Luiz P. Leal <brenoluizleal@gmail.com>
2022-04-15 02:18:30 -0300

    White Spaces

--- samha.backend/src/main/java/com/brenoleal/persistence/IGenericRepository.java
+++ samha.backend/src/main/java/com/brenoleal/persistence/IGenericRepository.java
@@ -6,9 +6,13 @@ import java.util.List;
 public interface IGenericRepository {
 
     <ENTITY> List<ENTITY> findAll(Class<ENTITY> entityClass);
+
     <ENTITY, KEY extends Serializable> ENTITY get(Class<ENTITY> entityClass, KEY id);
+
     <ENTITY> ENTITY save(ENTITY entityClass);
+
     <ENTITY> void delete(ENTITY entityClass);
+
     <ENTITY> ENTITY update(ENTITY entity);
 
     <ENTITY> List<ENTITY> find(Class<ENTITY> entityClass, IQueryBuilder<ENTITY, ENTITY> queryBuilder);
