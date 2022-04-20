0db1a2fabf12cc1ae25f1c4cfe5735937e693dc4
Breno Luiz P. Leal <brenoluizleal@gmail.com>
2022-04-19 23:07:01 -0300

    Protótipo de table header

--- samha-frontend/src/app/shared/table/table.component.ts
+++ samha-frontend/src/app/shared/table/table.component.ts
@@ -3,6 +3,14 @@ import {Observable, of} from 'rxjs';
 import {DataService} from '../data.service';
 import {catchError, tap} from 'rxjs/operators';
 
+/**
+ * Este componente gera dinamicamente uma tabela de acordo com os parâmetros passados
+ * @param resource recebe o endpoint respectivo nos controllers do backend, exemplo: api/usuario, neste caso o resource seria apenas
+ * 'usuario', pois 'api/' é um prefixo da aplicação
+ * @param columns representa as colunas que serão mostradas na tabela, este parâmetro vêm de constantes exportadas no pacote meta-model
+ *
+ * @author Breno Leal -- 19/04/2022
+ */
 @Component({
   selector: 'samha-table',
   templateUrl: './table.component.html',
@@ -13,18 +21,20 @@ export class TableComponent implements OnInit {
   @Input() columns: any[];
   dataSource$: Observable<any>;
   displayedColumns: string[] = [];
+  toolbarHeader: string;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.defineDisplayedColumns()
     this.dataSource$ = this.carregarUsuarios();
+    this.defineToolbarHeader();
   }
 
   carregarUsuarios(){
     return this.dataService.getAll(this.resource).pipe(tap(
       next => {
-
+        console.log(next);
       },
       catchError(error => of([]))
     ))
@@ -36,5 +46,9 @@ export class TableComponent implements OnInit {
         this.displayedColumns.push(column.columnDef)
       }
     })
+  }
+
+  private defineToolbarHeader() {
+    this.toolbarHeader = this.resource.charAt(0).toUpperCase() + this.resource.slice(1);
   }
 }
