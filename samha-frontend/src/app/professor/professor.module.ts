1dc89246a09d12887f90f036a60fc135e212e66a
Breno Luiz P. Leal <brenoluizleal@gmail.com>
2022-05-04 22:34:11 -0300

    Crud Professor

--- samha-frontend/src/app/professor/professor.module.ts
+++ samha-frontend/src/app/professor/professor.module.ts
@@ -4,12 +4,11 @@ import {ProfessorRoutingModule} from './professor-routing.module';
 import {SharedModule} from '../shared/shared.module';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { ProfessorDetailComponent } from './professor-detail/professor-detail.component';
 import { RestricaoComponent } from './restricao/restricao.component';
 import { RestricaoListComponent } from './restricao/restricao-list/restricao-list.component';
 
 @NgModule({
-  declarations: [ProfessorFormComponent, ProfessorDetailComponent, RestricaoComponent, RestricaoListComponent],
+  declarations: [ProfessorFormComponent, RestricaoComponent, RestricaoListComponent],
   imports: [
     CommonModule,
     ProfessorRoutingModule,
