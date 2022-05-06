d7f2e66917630d8022aba5c9ddd4e1cbc855b655
Breno Luiz P. Leal <brenoluizleal@gmail.com>
2022-05-06 18:18:04 -0300

    AutoComplete Field

--- samha-frontend/src/app/shared/fields/autocomplete-field/autocomplete-field.component.spec.ts
+++ samha-frontend/src/app/shared/fields/autocomplete-field/autocomplete-field.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RestricaoComponent } from './restricao.component';
+import { AutocompleteFieldComponent } from './autocomplete-field.component';
 
-describe('RestricaoComponent', () => {
-  let component: RestricaoComponent;
-  let fixture: ComponentFixture<RestricaoComponent>;
+describe('AutocompleteFieldComponent', () => {
+  let component: AutocompleteFieldComponent;
+  let fixture: ComponentFixture<AutocompleteFieldComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RestricaoComponent ]
+      declarations: [ AutocompleteFieldComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RestricaoComponent);
+    fixture = TestBed.createComponent(AutocompleteFieldComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ samha-frontend/src/app/shared/fields/autocomplete-field/autocomplete-field.component.ts
@@ -0,0 +1,44 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {first, map, startWith} from 'rxjs/operators';
+import {DataService} from '../../service/data.service';
+
+@Component({
+  selector: 'samha-autocomplete-field',
+  templateUrl: './autocomplete-field.component.html',
+  styleUrls: ['./autocomplete-field.component.css']
+})
+export class AutocompleteFieldComponent implements OnInit {
+  @Input() resource: string;
+  @Input() control: FormControl;
+  @Input() label: string;
+  filteredOptions: Observable<any[]>;
+  list: any[];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getAll(this.resource).pipe(first()).subscribe(
+      next =>{
+        this.list = next;
+        this.filteredOptions = this.control.valueChanges.pipe(
+          startWith(''),
+          map(value => (typeof value === 'string' ? value : value.name)),
+          map(name => (name ? this._filter(name) : this.list.slice())),
+        );
+      }
+    )
+
+  }
+
+  displayFn(entity: any): string {
+    return entity && entity.nome ? entity.nome : '';
+  }
+
+  private _filter(nome: string): any[] {
+    const filterValue = nome.toLowerCase();
+
+    return this.list.filter(entity => entity.nome.toLowerCase().includes(filterValue));
+  }
+}
