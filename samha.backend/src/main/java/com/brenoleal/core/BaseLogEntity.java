fa1217e9719918840770e871aa23565ba3787552
Breno Luiz P. Leal <brenoluizleal@gmail.com>
2022-05-11 18:26:30 -0300

    Mescla de logs

--- samha.backend/src/main/java/com/brenoleal/core/BaseLogEntity.java
+++ samha.backend/src/main/java/com/brenoleal/core/BaseLogEntity.java
@@ -1,5 +1,6 @@
 package com.brenoleal.core;
 
+import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -12,7 +13,19 @@ import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+/**
+ * Para a implementação desta classe, foi realizada uma junção de Hibernate Envers com o Spring Entity Listeners.
+ * O Hibernate Envers usa a tag @Audited para gerar uma tabela com os mesmo campos que a entidade mapeada e insere automáticamente
+ * os registros nesta tabela com um nível de versionamento.
+ * Já o Entity Listener cria estes quatro campos abaixo na entidade mapeada tornando possível saber a data das alterações
+ * e o autor destas. Juntando as duas soluções em uma mesma classe além de termos os campos abaixos na entidade mapeada
+ * também temos eles na tabela de auditoria criada (sufixo _aud), tornando possível fazer um gerenciamento avançado das alterações.
+ *
+ * O Ponto negativo é que os quatro campos abaixo também são replicados para as entidades mapeadas sem o sufixo _aud.
+ */
+
 @MappedSuperclass
+@Audited
 @EntityListeners(AuditingEntityListener.class)
 public class BaseLogEntity implements Serializable {
 
