54316e4791dc4e32f69c97eecee0d0e648f38a7c
Breno Leal <brenoluizleal@gmail.com>
2024-03-11 17:33:58 -0300

    Evitar entrys duplicados na criação de arquivos .zip

--- samha.backend/src/main/java/com/samha/util/Zipper.java
+++ samha.backend/src/main/java/com/samha/util/Zipper.java
@@ -1,41 +1,27 @@
 package com.samha.util;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zipper {
-    public static byte[] zipReports(List<byte[]> reports) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try (ZipOutputStream zipStream = new ZipOutputStream(outputStream)) {
-            int reportIndex = 1;
-            for (byte[] report : reports) {
-                String entryName = "report" + reportIndex + ".txt";
-                zipStream.putNextEntry(new ZipEntry(entryName));
-                zipStream.write(report);
-                zipStream.closeEntry();
-                reportIndex++;
-            }
-        }
-        OutputStream outputStream1 = new FileOutputStream("teste.zip");
-        outputStream1.write(outputStream.toByteArray());
-        return outputStream.toByteArray();
-    }
-
     public static byte[] createZipFile(List<Map<String, Object>> results) {
 
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
              ZipOutputStream zos = new ZipOutputStream(baos)) {
 
+            List<String> usedEntrys = new ArrayList<>();
             for (int i = 0; i < results.size(); i++) {
                 Map<String, Object> doc = results.get(i);
                 byte[] fileBytes = (byte[]) doc.get("bytes");
                 String fileName = doc.get("nome") + ".pdf";
+                if (usedEntrys.contains(fileName)) {
+                    fileName += "_" + i;
+                }else usedEntrys.add(fileName);
 
                 ZipEntry zipEntry = new ZipEntry(fileName);
                 zos.putNextEntry(zipEntry);
